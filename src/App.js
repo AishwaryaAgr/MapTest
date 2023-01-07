e756937c4cc0d7ae3476572bcafdecf8ecb4623b
AishwaryaAgr <Aishwarya@23484>
2023-01-07 17:28:40 +0530

    Electriva Becil

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import MyMapComponent from "./Map";
 import React, {useEffect} from 'react'
 import axios from "axios";
+import { 
+  lookupRegion, 
+} from "@googlemaps/region-lookup";
+
+const headers = {
+    "X-Goog-Api-Key": "AIzaSyAc46UYffrBosZmMVFEN1r0-IaIjytv8n0",
+  };
+
+const data= {
+  identifiers: [
+    {
+      place: "newark",
+      place_type: "locality" ,
+      region_code: "us",
+      language: "en",
+    },
+  ],
+};
+
 
 function App() {
 
   useEffect(() => {
-    axios({
-    url: `https://0j7fb0obf1.execute-api.ap-south-1.amazonaws.com/test/staff/tasks`,
-    method: 'POST',
-    data: {Ids: [1,9]}
-  })
-    .then(res=> console.log(res))
-    .catch(err=> console.log(err))
 
+      lookupRegion({ headers, data })
+      .then(res=>{
+        console.log(res.data);
+      })
+      .catch(err=>{
+        console.log(err);
+      })
+    
   }, [])
 
     return (
